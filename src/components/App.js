f7a3fc3d4d2a5060c023cb7cf7785be6062be63f
tanvirch0750 <tanvir0750@yahoo.com>
2021-11-01 21:51:04 +0600

    setup firebase and implemented authentication

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { AuthProvider } from "../context/AuthContext";
 import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
 import Quiz from "../pages/quiz/Quiz";
@@ -10,15 +11,17 @@ import Layout from "./layout/Layout";
 function App() {
   return (
     <Router>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/quiz" component={Quiz} />
-          <Route exact path="/result" component={Result} />
-        </Switch>
-      </Layout>
+      <AuthProvider>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/quiz" component={Quiz} />
+            <Route exact path="/result" component={Result} />
+          </Switch>
+        </Layout>
+      </AuthProvider>
     </Router>
   );
 }
